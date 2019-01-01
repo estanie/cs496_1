452cf7a41659d3549d1de27e2e5f91f2cb836fc6
estanie <ygy8463@gmail.com>
2019-01-02 01:00:04 +0900

    Changes design.

--- app/src/main/java/com/example/q/cs496_1/fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/q/cs496_1/fragments/GalleryFragment.kt
@@ -25,7 +25,7 @@ import java.util.*
 
 class GalleryFragment: Fragment() {
     private var adapter: ImageAdapter? = null
-
+    private var IMAGE_CAPTURED = false
     val REQUEST_IMAGE_CAPTURE = 1
     var mCurrentPhotoPath = ""
     val IMAGE_PATH = "/storage/emulated/0/DCIM/cs496_1"
@@ -48,8 +48,15 @@ class GalleryFragment: Fragment() {
         return view
     }
 
+    override fun onStart() {
+        super.onStart()
+        galleryAddPic()
+        Log.e("On START", "START!")
+    }
+
     // TODO(@estanie): It would be seperate to camera utils, Also do not use this way T-T. :0
     private fun dispatchTakePictureIntent() {
+        IMAGE_CAPTURED = true
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {takePictureIntent ->
             takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
                 val photoFile: File? = try {
@@ -62,7 +69,6 @@ class GalleryFragment: Fragment() {
                     val photoURI: Uri = FileProvider.getUriForFile(context!!, context!!.packageName+".fileprovider", it)
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                    galleryAddPic()
                 }
             }
         }
@@ -79,12 +85,16 @@ class GalleryFragment: Fragment() {
     }
 
     private fun galleryAddPic() {
+        if (!IMAGE_CAPTURED) return
         Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also {mediaScanIntent ->
             val f = File(mCurrentPhotoPath)
             mediaScanIntent.data = Uri.fromFile(f)
             Log.e("mCurrentPhotoPath", mediaScanIntent.data.path+"")
+            val checkFile = File(mediaScanIntent.data.path)
             context!!.sendBroadcast(mediaScanIntent)
+            if (!checkFile.exists()) return
             adapter!!.addImageToList(mCurrentPhotoPath)
+            IMAGE_CAPTURED = false
             // TODO(@estanie): If take picture canceled, remove this.
         }
     }
